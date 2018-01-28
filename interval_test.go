049655e4bff05195d6684b9ec9205764caff5dac
Dan McCandless <dkmccandless@gmail.com>
2018-01-28 12:28:00 +0800

    add Contains

--- interval_test.go
+++ interval_test.go
@@ -1,6 +1,8 @@
 package interval
 
-import "testing"
+import (
+	"testing"
+)
 
 var boolTests = []struct {
 	in                                                Interval
@@ -16,6 +18,9 @@ var boolTests = []struct {
 	{Interval{1, 1, Closed}, false, false, true, false, true, true},
 	{Interval{1, 2, Closed}, false, false, false, false, true, true},
 	{Interval{-1, 1, Closed}, false, true, false, false, true, true},
+	{Interval{0, inf, LeftClosed}, false, false, false, false, true, false},
+	{Interval{neginf, 0, Open}, false, false, false, false, false, false},
+	{Interval{neginf, inf, Open}, false, true, false, false, false, false},
 }
 
 func TestIsEmpty(t *testing.T) {
@@ -62,6 +67,50 @@ func TestRightIsClosed(t *testing.T) {
 	for _, test := range boolTests {
 		if got := test.in.RightIsClosed(); got != test.rightClosed {
 			t.Errorf("RightIsClosed(%v): got %v, want %v", test.in, got, test.rightClosed)
+		}
+	}
+}
+
+func TestContains(t *testing.T) {
+	for _, test := range []struct {
+		in   Interval
+		x    float64
+		want bool
+	}{
+		{Interval{}, 0, false},
+		{Interval{0, 0, Open}, 0, false},
+		{Interval{0, 0, LeftClosed}, 0, false},
+		{Interval{0, 0, RightClosed}, 0, false},
+		{Interval{0, 0, Closed}, 0, true},
+		{Interval{1, -1, Closed}, 0, false}, // a>b
+		{Interval{2, 4, Open}, 2, false},
+		{Interval{2, 4, LeftClosed}, 2, true},
+		{Interval{2, 4, RightClosed}, 2, false},
+		{Interval{2, 4, Closed}, 2, true},
+		{Interval{2, 4, Open}, 2.718, true},
+		{Interval{2, 4, LeftClosed}, 2.718, true},
+		{Interval{2, 4, RightClosed}, 2.718, true},
+		{Interval{2, 4, Closed}, 2.718, true},
+		{Interval{2, 4, Open}, 4, false},
+		{Interval{2, 4, LeftClosed}, 4, false},
+		{Interval{2, 4, RightClosed}, 4, true},
+		{Interval{2, 4, Closed}, 4, true},
+		{Interval{2, 4, Open}, -3, false},
+		{Interval{2, 4, LeftClosed}, -3, false},
+		{Interval{2, 4, RightClosed}, -3, false},
+		{Interval{2, 4, Closed}, -3, false},
+		{Interval{neginf, 0, RightClosed}, -18, true},
+		{Interval{neginf, 0, RightClosed}, 0, true},
+		{Interval{neginf, 0, RightClosed}, 6, false},
+		{Interval{0, inf, LeftClosed}, -18, false},
+		{Interval{0, inf, LeftClosed}, 0, true},
+		{Interval{0, inf, LeftClosed}, 6, true},
+		{Interval{neginf, inf, Open}, -3, true},
+		{Interval{neginf, inf, Open}, neginf, false},
+		{Interval{neginf, inf, Open}, inf, false},
+	} {
+		if got := test.in.Contains(test.x); got != test.want {
+			t.Errorf("Contains(%v, %v): got %v, want %v", test.in, test.x, got, test.want)
 		}
 	}
 }
