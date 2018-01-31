78e5c3a8972fe0f409edf89642e2c9d0638b584f
Dan McCandless <dkmccandless@gmail.com>
2018-01-31 21:42:40 +0800

    add Mul

--- arith.go
+++ arith.go
@@ -2,6 +2,13 @@ package interval
 
 func (e Ends) flip() Ends { return e&leftEndMask<<1 + e&rightEndMask>>1 }
 
+func (in *Interval) isP0() bool  { return in.a == 0 && in.b > 0 }
+func (in *Interval) isP1() bool  { return in.a > 0 }
+func (in *Interval) isPos() bool { return in.isP1() || in.isP0() }
+func (in *Interval) isN0() bool  { return in.a < 0 && in.b == 0 }
+func (in *Interval) isN1() bool  { return in.b < 0 }
+func (in *Interval) isNeg() bool { return in.isN1() || in.isN0() }
+
 // Neg returns the additive inverse of x.
 func (in *Interval) Neg() *Interval {
 	return &Interval{-in.b, -in.a, in.ends.flip()}
@@ -23,4 +30,40 @@ func Sub(x, y *Interval) *Interval {
 		return empty()
 	}
 	return Add(x, y.Neg())
+}
+
+// Mul returns the product x*y.
+// TODO: Handle outward rounding
+func Mul(x, y *Interval) *Interval {
+	switch {
+	case x.IsEmpty() || y.IsEmpty():
+		return empty()
+	case x.IsZero() || y.IsZero():
+		return zero()
+	case x.isPos() && y.isPos():
+		e := x.ends & y.ends
+		if x.a == 0 && x.LeftIsClosed() || y.a == 0 && y.LeftIsClosed() {
+			e |= leftEndMask
+		}
+		return &Interval{x.a * y.a, x.b * y.b, e}
+	case x.isPos() && y.IsMixed():
+		if x.RightIsClosed() {
+			return &Interval{x.b * y.a, x.b * y.b, y.ends}
+		} else {
+			return &Interval{x.b * y.a, x.b * y.b, Open}
+		}
+	case x.IsMixed() && y.IsMixed():
+		return Union(
+			&Interval{x.a * y.b, x.b * y.b, x.ends&y.ends.flip()&leftEndMask + x.ends&y.ends&rightEndMask},
+			&Interval{x.b * y.a, x.a * y.a, x.ends.flip()&y.ends&leftEndMask + x.ends.flip()&y.ends.flip()&rightEndMask},
+		)
+	case x.IsMixed() && y.isPos():
+		return Mul(y, x)
+	case x.isNeg():
+		return Mul(x.Neg(), y).Neg()
+	case y.isNeg():
+		return Mul(y.Neg(), x).Neg()
+	default:
+		panic("unhandled case")
+	}
 }
--- arith_test.go
+++ arith_test.go
@@ -26,7 +26,7 @@ func TestNeg(t *testing.T) {
 
 var (
 	ine  = empty()
-	inz  = &Interval{0, 0, Closed}
+	inz  = zero()
 	inp0 = &Interval{0, 0.5, Closed}
 	inp1 = &Interval{1, 2, Closed}
 	inm  = &Interval{-2, 4, Closed}
@@ -34,26 +34,26 @@ var (
 	inn1 = &Interval{-8, -4, Closed}
 )
 
-var arithTests = []struct{ x, y, add, sub *Interval }{
-	{ine, inp1, ine, ine},
-	{inp1, ine, ine, ine},
-	{inp1, inz, inp1, inp1},
-	{inz, inp1, inp1, inp1.Neg()},
-	{inp0, inp1, &Interval{1, 2.5, Closed}, &Interval{-2, -0.5, Closed}},
-	{inp1, inp0, &Interval{1, 2.5, Closed}, &Interval{0.5, 2, Closed}},
-	{inp1, inm, &Interval{-1, 6, Closed}, &Interval{-3, 4, Closed}},
-	{inp1, inn0, &Interval{0.75, 2, Closed}, &Interval{1, 2.25, Closed}},
-	{inp1, inn1, &Interval{-7, -2, Closed}, &Interval{5, 10, Closed}},
-	{inm, inp1, &Interval{-1, 6, Closed}, &Interval{-4, 3, Closed}},
-	{inm, inp0, &Interval{-2, 4.5, Closed}, &Interval{-2.5, 4, Closed}},
-	{inm, inm, &Interval{-4, 8, Closed}, &Interval{-6, 6, Closed}},
-	{inm, inn0, &Interval{-2.25, 4, Closed}, &Interval{-2, 4.25, Closed}},
-	{inm, inn1, &Interval{-10, 0, Closed}, &Interval{2, 12, Closed}},
-	{inn1, inp1, &Interval{-7, -2, Closed}, &Interval{-10, -5, Closed}},
-	{inn1, inp0, &Interval{-8, -3.5, Closed}, &Interval{-8.5, -4, Closed}},
-	{inn1, inm, &Interval{-10, 0, Closed}, &Interval{-12, -2, Closed}},
-	{inn1, inn0, &Interval{-8.25, -4, Closed}, &Interval{-8, -3.75, Closed}},
-	{inn0, inn1, &Interval{-8.25, -4, Closed}, &Interval{3.75, 8, Closed}},
+var arithTests = []struct{ x, y, add, sub, mul *Interval }{
+	{ine, inp1, ine, ine, ine},
+	{inp1, ine, ine, ine, ine},
+	{inp1, inz, inp1, inp1, inz},
+	{inz, inp1, inp1, inp1.Neg(), inz},
+	{inp0, inp1, &Interval{1, 2.5, Closed}, &Interval{-2, -0.5, Closed}, &Interval{0, 1, Closed}},
+	{inp1, inp0, &Interval{1, 2.5, Closed}, &Interval{0.5, 2, Closed}, &Interval{0, 1, Closed}},
+	{inp1, inm, &Interval{-1, 6, Closed}, &Interval{-3, 4, Closed}, &Interval{-4, 8, Closed}},
+	{inp1, inn0, &Interval{0.75, 2, Closed}, &Interval{1, 2.25, Closed}, &Interval{-0.5, 0, Closed}},
+	{inp1, inn1, &Interval{-7, -2, Closed}, &Interval{5, 10, Closed}, &Interval{-16, -4, Closed}},
+	{inm, inp1, &Interval{-1, 6, Closed}, &Interval{-4, 3, Closed}, &Interval{-4, 8, Closed}},
+	{inm, inp0, &Interval{-2, 4.5, Closed}, &Interval{-2.5, 4, Closed}, &Interval{-1, 2, Closed}},
+	{inm, inm, &Interval{-4, 8, Closed}, &Interval{-6, 6, Closed}, &Interval{-8, 16, Closed}},
+	{inm, inn0, &Interval{-2.25, 4, Closed}, &Interval{-2, 4.25, Closed}, &Interval{-1, 0.5, Closed}},
+	{inm, inn1, &Interval{-10, 0, Closed}, &Interval{2, 12, Closed}, &Interval{-32, 16, Closed}},
+	{inn1, inp1, &Interval{-7, -2, Closed}, &Interval{-10, -5, Closed}, &Interval{-16, -4, Closed}},
+	{inn1, inp0, &Interval{-8, -3.5, Closed}, &Interval{-8.5, -4, Closed}, &Interval{-4, 0, Closed}},
+	{inn1, inm, &Interval{-10, 0, Closed}, &Interval{-12, -2, Closed}, &Interval{-32, 16, Closed}},
+	{inn1, inn0, &Interval{-8.25, -4, Closed}, &Interval{-8, -3.75, Closed}, &Interval{0, 2, Closed}},
+	{inn0, inn1, &Interval{-8.25, -4, Closed}, &Interval{3.75, 8, Closed}, &Interval{0, 2, Closed}},
 }
 
 func TestAdd(t *testing.T) {
@@ -68,6 +68,14 @@ func TestSub(t *testing.T) {
 	for _, test := range arithTests {
 		if got := Sub(test.x, test.y); !Equal(got, test.sub) {
 			t.Errorf("Sub(%v, %v): got %v, want %v", test.x, test.y, got, test.sub)
+		}
+	}
+}
+
+func TestMul(t *testing.T) {
+	for _, test := range arithTests {
+		if got := Mul(test.x, test.y); !Equal(got, test.mul) {
+			t.Errorf("Mul(%v, %v): got %v, want %v", test.x, test.y, got, test.mul)
 		}
 	}
 }
